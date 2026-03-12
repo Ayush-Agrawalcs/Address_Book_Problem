7f5b33d68351c41e774ae6cce853dd206d1132b9
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-03-12 12:18:38 +0530

    Add:maintain multiple contact persons in Address Book

--- Address_book_main.py
+++ Address_book_main.py
@@ -1,45 +1,55 @@
 from Address_Book import address_book
 from Contact import contact
-from Helper.Edit import edit, delete
+from Helper.Edit import edit
 
 from Helper.Delete import delete
 
 
 book = address_book()
-First_Name=input("Enter your first name:")
-Last_Name=input("Enter your Last name:")
-Address=input("Enter your address:")
-City=input("Enter your city:")
-State=input("Enter your state:")
-Zip=input("Enter your zip code:")
-Phone_Number=input("Enter your phone number:")
-Email=input("Enter your email:")
+
+while(True):
+    First_Name=input("Enter your first name:")
+    Last_Name=input("Enter your Last name:")
+    Address=input("Enter your address:")
+    City=input("Enter your city:")
+    State=input("Enter your state:")
+    Zip=input("Enter your zip code:")
+    Phone_Number=input("Enter your phone number:")
+    Email=input("Enter your email:")
 
 
-c1 = contact(
-    First_Name,
-    Last_Name,
-    Address,
-    City,
-    State,
-    Zip,
-    Phone_Number,
-    Email
-)
-print("--------------")
+    c1 = contact(
+        First_Name,
+        Last_Name,
+        Address,
+        City,
+        State,
+        Zip,
+        Phone_Number,
+        Email
+    )
+    book.add_contact(c1)
+    print("Contact added successfully!")
+    print("1. Display Contacts")
+    print("2. Edit Contact")
+    print("3. Delete Contact")
+    print("0. Exit")
+    choice = int(input("Enter your choice: "))
+    match choice:
+        case 1:
+            book.display_contacts()
+            print("------------------------")
+        case 2:
+            name=input("Enter the name of the contact you want to edit:")
+            edit(book,name) 
+            book.display_contacts()
+            print("------------------------")
+        case 3:            
+            name=input("Enter the name of the contact you want to delete:")
+            delete(book,name)
+        case 0:
+            break
 
-book.add_contact(c1)
-book.display_contacts()
-
-s=input("you want to edit y/n")
-if(s.lower()=='y'):
-    name=input("Enter the name of the contact you want to edit:")
-    edit(book,name)
-book.display_contacts()
-
-
-print("---------")
-s=input("you want to Delete y/n")
-if(s.lower()=='y'):
-    name=input("Enter the name of the contact you want to edit:")
-    delete(book,name)
+    k=input("Enter N to Exit ")
+    if k.lower()=='n':
+        break
