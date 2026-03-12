b87f04545e6c613362767fb456cdb61753f30e02
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-03-12 10:20:57 +0530

    create a Address book class to add a user

--- Address_Book.py
+++ Address_Book.py
@@ -1,1 +1,48 @@
 print("Welcome to the Address Book!")
+
+class contact:
+    def __init__(self,first_name,last_name,address,city,state,zip,phone_number,email):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.address=address
+        self.city=city
+        self.state=state
+        self.zip=zip
+        self.phone_number=phone_number
+        self.email=email
+    def display(self):
+        print("First Name:", self.first_name)
+        print("Last Name:", self.last_name)
+        print("Address:", self.address)
+        print("City:", self.city)
+        print("State:", self.state)
+        print("Zip:", self.zip)
+        print("Phone:", self.phone_number)
+        print("Email:", self.email)
+
+class address_book:
+    def __init__(self):
+        self.contact=[]
+    def add_contact(self,contact):
+        self.contact.append(contact)
+    def display_contacts(self):
+        for contact in self.contact:
+            contact.display()
+            print("-------------")
+
+
+book = address_book()
+
+c1 = contact(
+    "Ayush",
+    "Agrawal",
+    "FF Road",
+    "Firozabad",
+    "Uttar Pradesh",
+    "283203",
+    "9045735010",
+    "ayu@gmail.com"
+)
+
+book.add_contact(c1)
+book.display_contacts()
